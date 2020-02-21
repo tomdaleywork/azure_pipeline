07e916dd02ccab1359b88c59355da94f3ff336f6
Tom Daley <thomas.daley@nhs.net>
2020-02-21 16:49:49 +0000

    update yaml

--- integration.py
+++ integration.py
@@ -1,7 +1,8 @@
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
 
-driver = Chrome(executable_path='./chromedriver.exe')
+# driver = Chrome(executable_path='./chromedriver.exe')
+driver = Chrome('/usr/bin/chromedriver')
 driver.get('http://127.0.0.1:5000')
 val = driver.find_element(By.ID, 'value')
 assert val.text == '15'
