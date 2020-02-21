f3e287ec672b6e03aaeca6b7673acc035813eb4e
Tom Daley <thomas.daley@nhs.net>
2020-02-21 16:59:05 +0000

    set chrome options

--- integration.py
+++ integration.py
@@ -1,8 +1,13 @@
-from selenium.webdriver import Chrome
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+
 # driver = Chrome(executable_path='./chromedriver.exe')
-driver = Chrome('/usr/bin/chromedriver')
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome('/usr/bin/chromedriver', chrome_options=chrome_options)
 driver.get('http://127.0.0.1:5000')
 val = driver.find_element(By.ID, 'value')
 assert val.text == '15'
