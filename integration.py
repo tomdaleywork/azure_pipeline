fc8b9d3ea3ea69e46bc47541fbc00e53611ed9b3
Tom Daley <thomas.daley@nhs.net>
2020-02-21 16:24:57 +0000

    WIP make selenium work

---
+++ integration.py
@@ -0,0 +1,10 @@
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
+
+driver = Chrome(executable_path='./chromedriver.exe')
+driver.get('http://127.0.0.1:5000')
+val = driver.find_element(By.ID, 'value')
+assert val.text == '15'
+print('Test Complete')
+
+driver.quit()
--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  value = addition(5, 10)
+  return f'<html><body><h1 id="value">{value}</h1></body></html>'
+
+
 def addition(x, y):
   print(f'addition called with {x}, {y}')
   return x + y
