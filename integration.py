4cdf9e28bb7be07a6f88954c15b69885e4790265
Tom Daley <thomas.daley@nhs.net>
2020-02-25 09:45:58 +0000

    refactoring the yaml

--- integration.py
+++ integration.py
@@ -7,7 +7,7 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome('/usr/bin/chromedriver', chrome_options=chrome_options)
+driver = webdriver.Chrome('/usr/bin/chromedriver', options=chrome_options)
 driver.get('http://127.0.0.1:5000')
 val = driver.find_element(By.ID, 'value')
 assert val.text == '15'
